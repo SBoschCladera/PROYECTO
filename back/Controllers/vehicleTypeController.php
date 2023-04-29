0082e7984381ce9517a03eb53ef40d803e51845a
SBoschCladera <73190236+SBoschCladera@users.noreply.github.com>
2023-04-29 18:14:27 +0200

    Añadido controlado backEnd
    Añadido controlado backEnd para los tipos de vehículos

---
+++ back/Controllers/vehicleTypeController.php
@@ -0,0 +1,23 @@
+<?php
+include_once "../Models/AdvertisementListModel.php";
+
+$model = new AdvertisementListModel();
+$vehicleType = $model->getVehicleTypes();
+
+$return = array(
+    "vehicleTypes" => $model->getVehicleTypes(),
+    "selectedVehicleTypeId" => "",
+    "selectedVehicleType" => "",
+);
+if (isset($_GET["id"])) {
+    $selectedBrand = $model->getVehicleType($_GET["id"]);
+    $return = array(
+        "vehicleTypes" => $model->getVehicleTypes(),
+        "selectedVehicleTypeId" => $_GET["id"],
+        "selectedVehicleType" => $selectedVehicleType
+    );
+}
+
+echo json_encode($return);
+
+    ?>
