4f7311ab11c6c39c07e2a124a55f1c5a4a773576
SBoschCladera <73190236+SBoschCladera@users.noreply.github.com>
2023-04-29 18:12:58 +0200

    Añadido controlado backEnd
    Añadido controlador backend para las marcas

---
+++ back/Controllers/brandController.php
@@ -0,0 +1,23 @@
+<?php
+include_once "../Models/AdvertisementListModel.php";
+
+$model = new AdvertisementListModel();
+$brands = $model->getBrands();
+
+$return = array(
+    "brands" => $model->getBrands(),
+    "selectedBrandId" => "",
+    "selectedBrand" => "",
+);
+if (isset($_GET["id"])) {
+    $selectedBrand = $model->getBrand($_GET["id"]);
+    $return = array(
+        "brands" => $model->getAdvertisements(),
+        "selectedBrandId" => $_GET["id"],
+        "selectedBrand" => $selectedBrand
+    );
+}
+
+echo json_encode($return);
+
+    ?>
