f3300f8f781ec1028527b1ce3e01e0e2f9e8fdc9
SBoschCladera <73190236+SBoschCladera@users.noreply.github.com>
2023-05-28 19:21:30 +0200

    Nuevo select para filtros de búsqueda

--- front/JS/search.js
+++ front/JS/search.js
@@ -1,21 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("searchForm").addEventListener("submit", function (event) {
-        event.preventDefault(); // Evitar el envío del formulario por defecto
+        event.preventDefault();
 
-        // Obtener el valor del campo de entrada
+        // Obtener el valor del campo de búsqueda
         let searchTerm = document.getElementById("searchInput").value;
 
-        // Realizar la solicitud AJAX al controlador del back-end
+        // Obtener el valor seleccionado del <select>
+        let selectElement = document.getElementById("searchSelect");
+        let selectedOption = selectElement.options[selectElement.selectedIndex];
+        let selectedValue = selectedOption.value;
+
+        // Crear un objeto FormData para enviar los datos por POST
+        let formData = new FormData();
+        formData.append("param", searchTerm);
+        formData.append("selectParam", selectedValue);
+
+        // Realizar la solicitud AJAX al controlador del backend
         let xhr = new XMLHttpRequest();
-        xhr.open("GET", "../../back/Controllers/searcherController.php?param=" + encodeURIComponent(searchTerm), true);
+        xhr.open("POST", "../../back/Controllers/listController.php", true);
         xhr.onreadystatechange = function () {
+
+            let counter = 0;
+
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
                     // Procesar la respuesta del controlador
                     let data = JSON.parse(xhr.responseText);
-                    let advertisements = data.advertisements;
-
-                    //let divCarousel = document.querySelectorAll(".carouselClass");
 
                     // Buscar el div que deseas verificar por su clase
                     let divToDelete = document.getElementById("notFoundMatchesDiv");
@@ -33,16 +43,153 @@ document.addEventListener("DOMContentLoaded", function () {
                     for (let j = 0; j < divCarousel.length; j++) {
                         divCarousel[j].remove();
                     }
-                    makeList(data, advertisements.length);                   
+
+                    // Capitaliza un string para adecuarlo a la salida de base de datos
+                    let stringToCapitalize = searchTerm;
+                    let capitalizeSearchTerm = stringToCapitalize.charAt(0).toUpperCase() + stringToCapitalize.slice(1).toLowerCase();
+
+                    // Búsqueda según la opción seleccionada.
+                    switch (selectedValue) {
+                        case "1":
+                            for (let i = 0; i < data.advertisements.length; i++) {
+                                if (data.advertisements[i].brand_id.name == capitalizeSearchTerm) {
+                                    alignedDiv(counter);
+                                    counter++;
+                                    makeCard(i, i, data.advertisements);
+                                }
+                            }
+                            notResults(counter);
+                            break;
+                        case "2":
+                            for (let i = 0; i < data.advertisements.length; i++) {
+                                if (data.advertisements[i].model_id.name == capitalizeSearchTerm) {
+                                    alignedDiv(counter);
+                                    counter++;
+                                    makeCard(i, i, data.advertisements);
+                                }
+                            }
+                            notResults(counter);
+                            break;
+                        case "3":
+                            for (let i = 0; i < data.advertisements.length; i++) {
+                                if (data.advertisements[i].engine_type_id.name == capitalizeSearchTerm) {
+                                    alignedDiv(counter);
+                                    counter++;
+                                    makeCard(i, i, data.advertisements);
+                                }
+                            }
+                            notResults(counter);
+                            break;
+                        case "4":
+                            for (let i = 0; i < data.advertisements.length; i++) {
+                                if (data.advertisements[i].color == searchTerm) {
+                                    alignedDiv(counter);
+                                    counter++;
+                                    makeCard(i, i, data.advertisements);
+                                }
+                            }
+                            notResults(counter);
+                            break;
+                        case "5":
+                            for (let i = 0; i < data.advertisements.length; i++) {
+                                if (data.advertisements[i].motorization_id.power == capitalizeSearchTerm) {
+                                    alignedDiv(counter);
+                                    counter++;
+                                    makeCard(i, i, data.advertisements);
+                                }
+                            }
+                            notResults(counter);
+                            break;
+                        case "6":
+                            for (let i = 0; i < data.advertisements.length; i++) {
+                                if (data.advertisements[i].benefits_id.max_velocity == searchTerm) {
+                                    alignedDiv(counter);
+                                    counter++;
+                                    makeCard(i, i, data.advertisements);
+                                }
+                            }
+                            notResults(counter);
+                            break;
+                        case "7":
+                            for (let i = 0; i < data.advertisements.length; i++) {
+                                if (data.advertisements[i].benefits_id.acceleration_0_100 == searchTerm) {
+                                    alignedDiv(counter);
+                                    counter++;
+                                    makeCard(i, i, data.advertisements);
+                                }
+                            }
+                            notResults(counter);
+                            break;
+                        case "8":
+                            for (let i = 0; i < data.advertisements.length; i++) {
+                                if (data.advertisements[i].benefits_id.consumption == searchTerm) {
+                                    alignedDiv(counter);
+                                    counter++;
+                                    makeCard(i, i, data.advertisements);
+                                }
+                            }
+                            notResults(counter);
+                            break;
+                        case "9":
+                            for (let i = 0; i < data.advertisements.length; i++) {
+                                if (data.advertisements[i].km == searchTerm) {
+                                    alignedDiv(counter);
+                                    counter++;
+                                    makeCard(i, i, data.advertisements);
+                                }
+                            }
+                            notResults(counter);
+                            break;
+                        case "10":
+                            for (let i = 0; i < data.advertisements.length; i++) {
+                                if (data.advertisements[i].brand_id.country.name == capitalizeSearchTerm) {
+                                    alignedDiv(counter);
+                                    counter++;
+                                    makeCard(i, i, data.advertisements);
+                                }
+                            }
+                            notResults(counter);
+                            break;
+                        case "11":
+                            for (let i = 0; i < data.advertisements.length; i++) {
+                                let dateString = data.advertisements[i].model_id.releaseYear.date;
+                                let year = dateString.slice(0, 4);
+                                if (year == searchTerm) {
+                                    alignedDiv(counter);
+                                    counter++;
+                                    makeCard(i, i, data.advertisements);
+                                }
+                            }
+                            notResults(counter);
+                            break;
+                        case "12":
+                            for (let i = 0; i < data.advertisements.length; i++) {
+                                if (data.advertisements[i].price == searchTerm) {
+                                    alignedDiv(counter);
+                                    counter++;
+                                    makeCard(i, i, data.advertisements);
+                                } else { }
+                            }
+                            notResults(counter);
+                            break;
+                        default:
+                            errorMessageModal('Algo no ha ido bien...', 'Debes seleccionar un filtro para encontrar resultados.');
+                            deleteShowErrorModal();
+                    }
+
+                    // Reinicia los valores de búsqueda
+                    selectElement.value = "0";
+                    document.getElementById("searchInput").value = "";
 
                 } else {
                     console.error("Error en la solicitud AJAX. Estado: " + xhr.status);
-                } 
+                }
                 // Añade o elimina la clase 'dark-mode'
-                    darkModeCards();
+                darkModeCards();
             }
         };
-        xhr.send();
+        xhr.send(formData);
     });
 });
+
 
